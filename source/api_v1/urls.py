01d9fec0009e2b1ebd5fb7a4960dbf558b31c517
Kirill Grekhovodov <kirill@wildlabs.co>
2025-08-19 20:57:48 +0600

    добавил обработку пост запросов

--- source/api_v1/urls.py
+++ source/api_v1/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
-from api_v1.views import echo
+from api_v1.views import echo, articles, get_token_view
 
 app_name = 'v1'
 
 urlpatterns = [
     path('echo/', echo, name='echo'),
+    path('articles/', articles, name='articles'),
+    path('get-csrf/', get_token_view, name='get-csrf'),
+
 ]
--- source/api_v1/views.py
+++ source/api_v1/views.py
@@ -1,22 +1,66 @@
+import json
 from datetime import datetime
 from decimal import Decimal
+from json import JSONDecodeError
 
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponseNotAllowed, HttpResponse, HttpResponseBadRequest
 
 from webapp.models import Article
 
+from django.views.decorators.csrf import ensure_csrf_cookie
+
+
+@ensure_csrf_cookie
+def get_token_view(request, *args, **kwargs):
+    if request.method == 'GET':
+        return HttpResponse()
+    return HttpResponseNotAllowed('Only GET request are allowed')
+
 
 def echo(request):
-    article = Article.objects.first()
-    test = {
-        "my_list": [1,2,3],
-        "price": Decimal('10.00'),
-        "datetime": datetime.now(),
-        "article": {
-            "title": article.title,
-            "content": article.content,
-            "author": article.author.username,
+    if request.method == 'GET':
+        article = Article.objects.first()
+        test = {
+            "my_list": [1,2,3],
+            "price": Decimal('10.00'),
+            "datetime": datetime.now(),
+            "article": {
+                "title": article.title,
+                "content": article.content,
+                "author": article.author.username,
+            }
         }
-    }
-    return JsonResponse(test)
+        return JsonResponse(test)
+    elif request.method == 'POST':
+        body = json.loads(request.body)
+        return JsonResponse(body)
+    else:
+        return HttpResponseNotAllowed(['GET', 'POST'])
+
+
+
+def articles(request):
+    if request.method == 'GET':
+        articles = Article.objects.all()
+
+        response_articles = []
+
+        for article in articles:
+            response_articles.append({
+                "title": article.title,
+                "content": article.content,
+                "author": article.author.username,
+            })
+
+        return JsonResponse(response_articles, safe=False)
+    elif request.method == 'POST':
+        try:
+            print(request.user)
+            body = json.loads(request.body)
+            article = Article.objects.create(**body, author=request.user)
+            return JsonResponse({"id": article.id}, status=201)
+        except JSONDecodeError as e:
+            return HttpResponseBadRequest()
+    else:
+        return HttpResponseNotAllowed(['GET', 'POST'])
 
