c3af9e9cfefbdb9cba7ef3aa16fd8e1d3f99900f
Aleksei_Kuzmin <leha.21.05.1996@gmail.com>
2025-09-23 20:07:27 +0600

    обновил ALLOWED_HOSTS для разрешения доступа с конкретного IP-адреса

--- source/hello/settings.py
+++ source/hello/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env.str('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['104.248.188.112']
 
 
 # Application definition
